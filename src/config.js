4b2cca13149d5c5eb6d68ba81c188566dc847127
Yuri <yuri_dsx_@hotmail.com>
2021-03-03 16:58:00 -0300

    (add) env variables

---
+++ src/config.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({
+  path: path.resolve(__dirname, '../.env')
+})
+
+export const PORT = process.env.PORT || 3000
+export const DATABASE = process.env.DATABASE_URL
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
 import app from './app'
+import { PORT } from './config'
 
-app.listen(3000)
+app.listen(PORT)
