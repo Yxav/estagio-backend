2bc197c049252d71cd3623194e9480389ea1393b
Yuri <yuri_dsx_@hotmail.com>
2021-03-04 17:01:04 -0300

    (add) seeds files

--- src/database/seeds/01-projects.js
+++ src/database/seeds/01-projects.js
@@ -3,9 +3,11 @@ exports.seed = knex => {
   return knex('projects').del()
     .then(function () {
       return knex('projects').insert([
-        { id: 1, name: 'Nave Back-end boilerplate NodeJS' },
-        { id: 2, name: 'Nave Back-end boilerplate ReactJS' },
-        { id: 3, name: 'Nave initial starter pack' }
+        { id: 10, name: 'Nave Back-end boilerplate NodeJS' },
+        { id: 15, name: 'Nave Back-end boilerplate ReactJS' },
+        { id: 35, name: 'Nave initial starter pack' }
       ])
     })
 }
+
+// ids com valores diferentes, para não dar conflito de primary keys
--- src/database/seeds/03-project-navers.js
+++ src/database/seeds/03-project-navers.js
@@ -2,12 +2,8 @@ exports.seed = knex => {
   return knex('navers_projects').del()
     .then(() => {
       return knex('navers_projects').insert([
-        { project_id: 1, naver_id: 3 },
-        { project_id: 2, naver_id: 2 },
-        { project_id: 3, naver_id: 1 },
-        { project_id: 3, naver_id: 3 },
-        { project_id: 2, naver_id: 2 },
-        { project_id: 1, naver_id: 1 }
+        { project_id: 10, naver_id: 3 },
+        { project_id: 15, naver_id: 4 },
       ])
     })
 }
