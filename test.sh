cc147bffcf27cf5eb80fe61a624cf8999cba6895
Yuri <yuri_dsx_@hotmail.com>
2021-03-06 00:02:22 -0300

    (add) automate .env creating

--- test.sh
+++ test.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+sudo -u postgres psql <<EOF
+CREATE DATABASE navetest;
+CREATE USER nave WITH PASSWORD 'nave';
+GRANT ALL PRIVILEGES ON DATABASE navetest TO nave;
+EOF
+
+touch .env 
+echo "DATABASE = 'postgres://nave:nave@localhost:5432/navetest'
+PORT = 3000" >> .env 
+
+
+npm i 
 npm run db:migrate
 npm run db:seed
 npm start
