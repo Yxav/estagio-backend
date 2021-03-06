c1c9798d24e0a9e67f66e7051abd9ceae416581a
Yuri <yuri_dsx_@hotmail.com>
2021-03-06 00:03:55 -0300

    (fix) change ids to prevent conflict pkey on database

--- src/database/seeds/03-project-navers.js
+++ src/database/seeds/03-project-navers.js
@@ -2,8 +2,8 @@ exports.seed = knex => {
   return knex('navers_projects').del()
     .then(() => {
       return knex('navers_projects').insert([
-        { project_id: 10, naver_id: 3 },
-        { project_id: 15, naver_id: 4 },
+        { project_id: 10, naver_id: 30 },
+        { project_id: 15, naver_id: 40 },
       ])
     })
 }
