2d3beaea77c2edb4ea3f7f9bcb5617f7378ebd98
Yuri <yuri_dsx_@hotmail.com>
2021-03-06 03:54:37 -0300

    (fix) add extra ssl prod

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {connectionString:process.env.DATABASE_URL, ssl=true},
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/src/database/migrations',
     },
@@ -30,5 +30,8 @@ module.exports = {
       directory: __dirname + '/src/database/seeds',
     },
     useNullAsDefault: true,
+    extra: {
+      ssl: true
+ }
   },
 };
