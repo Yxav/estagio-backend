2f05cc4e3664770d7d8785a3dd9769b49013571c
Yuri <yuri_dsx_@hotmail.com>
2021-03-06 14:03:16 -0300

    (fix) prod ssl false

--- knexfile.js
+++ knexfile.js
@@ -22,8 +22,10 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
-    ssl: true,
+    connection: {
+      connectionString : process.env.DATABASE_URL,
+      ssl: false
+    },
     migrations: {
       directory: __dirname + '/src/database/migrations',
     },
